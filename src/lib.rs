dcd1ff00b577b98a9806b29fef99350ec607c972
P34RL <undeadgamer279@gmail.com>
2023-12-14 10:41:59 -0500

    improved errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashMap, ffi::OsString, os::windows::ffi::OsStringExt};
+#![deny(unsafe_op_in_unsafe_fn)]
+
+use std::{collections::HashMap, ffi::OsString, mem, os::windows::ffi::OsStringExt};
 
 use winapi::{
     shared::{minwindef, windef::HWND},
@@ -10,25 +12,36 @@ use winapi::{
 
 /// # Safety
 ///
-/// HWND should be a valid pointer to a program.
-pub unsafe fn set_borderless_fullscreen(hwnd: HWND) {
-    let mut rect = std::mem::zeroed();
-    unsafe { winuser::GetClientRect(hwnd, &mut rect) };
+/// HWND should be a valid memory address to a program.
+pub unsafe fn set_borderless_fullscreen(hwnd: HWND) -> Result<(), &'static str> {
+    let mut rect = unsafe { mem::zeroed() };
+
+    let err = unsafe { winuser::GetClientRect(hwnd, &mut rect) } == 0;
+    if err {
+        return Err("Failed to get client rect");
+    }
 
     let monitor = unsafe { winuser::MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST) };
-    let mut monitor_info: winuser::MONITORINFO = unsafe { std::mem::zeroed() };
-    monitor_info.cbSize = std::mem::size_of::<winuser::MONITORINFO>() as u32;
-    unsafe { winuser::GetMonitorInfoW(monitor, &mut monitor_info) };
+    let mut monitor_info: winuser::MONITORINFO = unsafe { mem::zeroed() };
+    monitor_info.cbSize = mem::size_of::<winuser::MONITORINFO>() as u32;
+
+    let err = unsafe { winuser::GetMonitorInfoW(monitor, &mut monitor_info) } == 0;
+    if err {
+        return Err("Failed to get monitor info");
+    }
 
     let monitor_width = monitor_info.rcMonitor.right - monitor_info.rcMonitor.left;
     let monitor_height = monitor_info.rcMonitor.bottom - monitor_info.rcMonitor.top;
 
     // Set the window style to borderless
     let style = WS_POPUP;
-    unsafe { winuser::SetWindowLongPtrW(hwnd, GWL_STYLE, style as _) };
+    let err = unsafe { winuser::SetWindowLongPtrW(hwnd, GWL_STYLE, style as _) } == 0;
+    if err {
+        return Err("Failed to set window style");
+    }
 
     // Set window position and size to cover the entire screen
-    unsafe {
+    let err = unsafe {
         winuser::SetWindowPos(
             hwnd,
             HWND_TOP,
@@ -38,16 +51,26 @@ pub unsafe fn set_borderless_fullscreen(hwnd: HWND) {
             monitor_height,
             SWP_FRAMECHANGED | SWP_SHOWWINDOW,
         )
-    };
+    } == 0;
+    if err {
+        return Err("Failed to set window position and size");
+    }
 
     // Maximize the window
-    unsafe { winuser::ShowWindow(hwnd, SW_MAXIMIZE) };
+    let err = unsafe { winuser::ShowWindow(hwnd, SW_MAXIMIZE) } == 0;
+    if err {
+        return Err("Failed to maximize window");
+    }
+
+    Ok(())
 }
 
 extern "system" fn enum_windows_proc(hwnd: HWND, l_param: minwindef::LPARAM) -> minwindef::BOOL {
-    let mut buffer = [0; 512];
+    const BUFFER_SIZE: usize = 512;
+
+    let mut buffer = [0; BUFFER_SIZE];
     let end_of_str =
-        unsafe { winuser::GetWindowTextW(hwnd, buffer.as_mut_ptr(), buffer.len() as i32) } as usize;
+        unsafe { winuser::GetWindowTextW(hwnd, buffer.as_mut_ptr(), BUFFER_SIZE as i32) } as usize;
 
     let window_title = OsString::from_wide(&buffer[..end_of_str])
         .into_string()
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 const MESSAGE: &str = "Which program would you like to borderlessly fullscreen?";
 
-fn main() -> Result<(), inquire::InquireError> {
+#[derive(Debug, derive_more::From)]
+enum MainErr {
+    InquireError(inquire::InquireError),
+    Static(&'static str),
+}
+
+fn main() -> Result<(), MainErr> {
     let all_windows = borderless_window_rs::read_all_windows();
 
     let args: Vec<String> = std::env::args().skip(1).collect();
     let target_program = args.join(" ");
 
     let target_program = if target_program.is_empty() {
-        inquire::Select::new(MESSAGE, all_windows.keys().collect::<Vec<_>>())
-            .prompt()?
-            .to_string()
+        inquire::Select::new(MESSAGE, all_windows.keys().collect::<Vec<_>>()).prompt()?
     } else {
-        target_program
+        &target_program
     };
 
-    if let Some(&hwnd) = all_windows.get(&target_program) {
-        unsafe { borderless_window_rs::set_borderless_fullscreen(hwnd) }
+    if let Some(&hwnd) = all_windows.get(target_program) {
+        unsafe { borderless_window_rs::set_borderless_fullscreen(hwnd)? }
     }
 
     Ok(())
